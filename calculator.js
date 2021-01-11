b0320a80fa09dbbd61f2592eba040e0cf6af0c0f
Rabab <robie8_@outlook.com>
2021-01-11 17:59:33 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,38 @@
 // Define the numbers and the operation here
 
+
+
+// take the operator input
+const operator = prompt("Enter operator +, -, * , / : ");
+prompt.get();
+
+
+// take the operand input
+const number1 = parseInt(prompt("Enter first number: "));
+const number2 = parseInt(prompt("Enter second number: "));
+
+let result;
+
 // Write if-statements here
+
+if (operator === "+") {
+    result = number1 + number2;
+    console.log(`${number1} + ${number2} = ${result}`);
+
+}
+else if (operator === "-") {
+    result = number1 - number2;
+    console.log(`${number1} - ${number2} = ${result}`);
+
+}
+else if (operator === "*") {
+    result = number1 * number2;
+    console.log(`${number1} * ${number2} = ${result}`);
+
+}
+else if (operator === "/")  {
+    result = number1 / number2;
+    console.log(`${number1} / ${number2} = ${result}`);
+
+}
+
